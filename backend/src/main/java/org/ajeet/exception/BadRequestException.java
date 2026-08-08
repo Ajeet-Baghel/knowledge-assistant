0146eae150ef3ceeebdcdf105b33d0034be1d119
Ajeet Baghel <ajeetbaghelcink@gmail.com>
2026-08-08 22:50:52 +0530

    Created BadRequest and Response Exception handling

--- backend/src/main/java/org/ajeet/exception/BadRequestException.java
+++ backend/src/main/java/org/ajeet/exception/BadRequestException.java
@@ -1,4 +1,8 @@
 package org.ajeet.exception;
 
-public class BadRequestException {
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
 }
