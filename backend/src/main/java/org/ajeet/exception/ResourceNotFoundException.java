741831d8b7a9ec3d55bdbc01f51d81f829b4c994
Ajeet Baghel <ajeetbaghelcink@gmail.com>
2026-08-08 22:46:40 +0530

    Created golabal exception handling

--- backend/src/main/java/org/ajeet/exception/ResourceNotFoundException.java
+++ backend/src/main/java/org/ajeet/exception/ResourceNotFoundException.java
@@ -1,4 +1,8 @@
 package org.ajeet.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
